6941a4283acbb5f5971db6b9d5f4a52ec875ec2c
abel <abeloabate01@gmail.com>
2024-09-10 19:12:40 +0300

    adding realtime logging

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,55 @@
-// components/SupabaseChecker.js
 'use client';
 
 import { useEffect, useReducer } from 'react';
 import { supabase } from '../lib/supabaseClient'
 
-interface data {
+interface Data {
   title: string;
+  id?: number,
+  Title?: string,
   description: string;
-}
-interface state {
-  title: string;
-  description: string;
-  data?: data[];
 }
 
-interface action {
-  type: 'fetch' | 'add' | 'reset',
+interface State {
+  title: string;
+  description: string;
+  data: Data[];
+  isLoading: boolean;
+}
+
+interface Action {
+  type: 'fetch' | 'add' | 'reset';
   title?: string;
   desc?: string;
-  payload?: data[];
+  payload?: Data[];
+  loading?: boolean;
 }
-const initialState = {
+
+const initialState: State = {
   data: [],
   title: '',
   description: '',
+  isLoading: false,
 }
-function reducer(state: state, action: action) {
+
+
+function reducer(state: State, action: Action): State {
   switch (action.type) {
     case 'fetch':
-      return { ...state, data: action.payload }
+      return { ...state, data: action.payload || [] }
     case 'add':
       return {
         ...state,
-        title: action.title || state.title,  // Update title or keep existing
-        description: action.desc || state.description, // Update description if provided
+        title: action.title || state.title,  // Use nullish coalescing operator
+        description: action.desc || state.description, // Use nullish coalescing operator
+        isLoading: action.loading || state.isLoading,
       }
     case 'reset':
       return {
         ...state,
         title: '',
         description: '',
+        isLoading: false,
       };
     default:
       return state;
@@ -47,7 +57,6 @@ function reducer(state: state, action: action) {
 }
 
 const Home = () => {
-
   const [state, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
@@ -56,30 +65,41 @@ const Home = () => {
       if (error) {
         console.log(error.message)
       } else {
-        dispatch({ type: 'fetch', payload: todoData })
+        dispatch({ type: 'fetch', payload: todoData || [] })
       }
     }
     fetchTodo();
-  }, [])
+
+    const channel = supabase.channel('public:todo')
+      .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'todo' }, (payload: { new: Data }) => {
+        dispatch({ type: 'fetch', payload: [...state.data, payload.new] })
+      })
+
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [state.data])
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+    e.preventDefault();
+    dispatch({ type: 'add', loading: true })
     const { title, description } = state;
-    const { data: todoData, error } = await supabase.from('todo').insert({ Title: title, Description: description }).select()
+    const { error } = await supabase.from('todo').insert({ Title: title, Description: description }).select()
     if (error) {
       console.log(error.message)
     } else {
-      console.log(todoData)
       dispatch({ type: 'reset' })
     }
   }
 
   return (
-    <div className="flex place-content-center flex-wrap w-screen h-screen bg-red-100">
+    <div className="flex place-content-center flex-wrap w-screen h-screen block  bg-red-100">
       <form className='bg-red-200 rounded-lg block basis-4/6 block lg:basis-1/3 h-fit p-8' onSubmit={handleSubmit}>
         <h3 className='font-mono block text-right text-2xl'>Create Todo</h3>
         <div className='basis-10/12 rounded-md bg-none m-2 h-fit p-2 '>
-          <label className=" input input-bordered flex items-center gap-2">
+          <label className="input input-bordered flex items-center gap-2">
             <svg fill="#000000" className='h-4 w-4 opacity-70'
               viewBox="0 0 306.637 306.637" >
               <g>
@@ -89,44 +109,23 @@ const Home = () => {
                   <path d="M251.329,0l-41.507,41.507l55.308,55.308l41.507-41.507L251.329,0z M231.035,41.507l20.294-20.294l34.095,34.095
                 L265.13,75.602L231.035,41.507z"/>
                 </g>
-                <g>
-                </g>
-                <g>
-                </g>
-                <g>
-                </g>
-                <g>
-                </g>
-                <g>
-                </g>
-                <g>
-                </g>
-                <g>
-                </g>
-                <g>
-                </g>
-                <g>
-                </g>
-                <g>
-                </g>
-                <g>
-                </g>
-                <g>
-                </g>
-                <g>
-                </g>
-                <g>
-                </g>
-                <g>
-                </g>
               </g>
             </svg>
-            <input type="text" value={state.title} onChange={(e) => dispatch({ type: 'add', title: e.target.value })} className="grow" placeholder="Title" />
+            <input type="text" value={state.title} onChange={(e) => dispatch({ type: 'add', title: e.target.value })} className="grow" placeholder="Title" required />
           </label>
           <textarea value={state.description} onChange={(e) => dispatch({ type: 'add', desc: e.target.value })} className="block textarea textarea-bordered textarea-md  mt-4 w-full" placeholder="Description (optional)"></textarea>
         </div>
-        <button type="submit" className="ml-auto btn block btn-active">Add</button>
+        <button type="submit" className="ml-auto btn block btn-active">{state.isLoading ? "sending.." : "add"}</button>
       </form>
+      <div className='bg-red-200 w-screen h-fit p-2'>
+        <ul>
+          {state.data.map((todos) =>
+          (
+            <li key={todos.id}>{todos.Title}</li>
+          )
+          )}
+        </ul>
+      </div>
     </div>
   );
 };
